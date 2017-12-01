aeb88aacbdac3ff5098d6c670a897b231d8b7fd3
JA@02697400 <jahlil.allen@bison.howard.edu>
2017-12-01 18:45:14 -0500

    Starting to use functions
    This is only one part of the functions that I am going to write

--- MIPSassignment2.s
+++ MIPSassignment2.s
@@ -1,14 +1,3 @@
 .data
-	
+	message: .asciiz "Hi, everybody.\nMy name is Jahlil.\n"
 .text
-	addi $t0, $zero, 2000
-	addi $t1, $zero, 10
-	
-	mult $t0, $t1
-	
-	mflo $s0
-	
-	li $v0, 1
-	add $a0, $zero, $s0
-	syscall
-
