9002e17479948a50b1a4e35cf339860b508883d9
JA@02697400 <jahlil.allen@bison.howard.edu>
2017-12-01 19:37:33 -0500

    Getting user input
    Setting up MIPS to receive an array of characters which is my string

--- MIPSassignment2.s
+++ MIPSassignment2.s
@@ -1,3 +1,21 @@
 .data
-	message: .asciiz "Hi, everybody.\nMy name is Jahlil.\n"
+	prompt: .asciiz "Enter 1000 characters: "
+	message: .asciiz "\nToo Large\n"
 .text
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	
+	li $v0, 8
+	syscall
+	
+	move $t0, $v0
+	
+	li $v0, 4
+	la $a0, message
+	syscall
+	
+	li $v0, 4
+	move $a0, $t0
+	syscall
+	
